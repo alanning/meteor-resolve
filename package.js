fb4faf57bd5fa9d03b3e208b592ae623c1174b17
alanning <alanning@gmail.com>
2016-02-16 01:39:48 -0500

    Include git repository url

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'alanning:resolve',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'Resolve a value given a path and an object',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/alanning/meteor-resolve',
   documentation: 'README.md'
 });
 
